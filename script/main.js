c01a6596668e9d58b99a6135668876cf3b239017
VyLoTe <antran064969@gmail.com>
2025-01-24 11:55:02 +0700

    last one

--- script/main.js
+++ script/main.js
@@ -224,7 +224,7 @@ const animationTimeline = () => {
         }, {
             scale: 1,
             rotationY: 0,
-            color: "#ff69b4",
+            color: "#0ed7d7",
             ease: Expo.easeOut,
         },
         0.1,
